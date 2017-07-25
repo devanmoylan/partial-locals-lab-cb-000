8d1e1ff947697d4b568b8868aa92ed6843ec069d
Devan <devan.moylan@backyardlion.com>
2017-07-25 13:28:22 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name_searched)
+    if name_searched.empty?
+      self.all
+    else
+      where("name LIKE ?", "%#{name_searched}%")
+    end
+  end
+
 end
